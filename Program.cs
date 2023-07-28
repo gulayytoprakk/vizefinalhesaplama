9a78c1c90743034c32baef8ec2c1c1471698fcd0
Gülay TOPRAK <140807385+gulayytoprakk@users.noreply.github.com>
2023-07-28 17:04:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("vize notunu giriniz:");
+        int vize=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("final notunu giriniz:");
+        int final=Convert.ToInt32(Console.ReadLine());
+
+        int ort = Convert.ToInt32(vize * 0.4 + final * 0.6);
+
+        Console.WriteLine("Ortalamanız = {0}", ort);
+
+        if (ort < 50) 
+        { 
+            Console.WriteLine("KALDINIZ");
+        }
+        else
+        {
+            Console.WriteLine("GEÇTİNİZ");
+        }
+        Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+    }
+}
